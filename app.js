b5be6f2a2844e6405bd9481a1291fc30137994c7
c_ymelpati <c_ymelpati@IM-PC-242.innomindshyd.com>
2018-02-05 17:01:21 +0530

    With added changes
    There will all validation messages once on submit.

--- app.js
+++ app.js
@@ -1,6 +1,53 @@
 var app = angular.module("myApp",[]);
 app.controller('myc', function ($scope) {
+    $scope.myvalidname = "hidden";
+    $scope.myvalidemail = "hidden";
+    $scope.myvalidaddress1 = "hidden";
+    $scope.myvalidzip = "hidden";
+    $scope.myvalidcity = "hidden";
+    $scope.myvalidstate = "hidden";
+    $scope.myvalidcountry = "hidden";
     $scope.submit = function(){
+        
+        if(!$scope.myForm.name.$touched || $scope.myForm.name.$invalid){
+            $scope.myvalidname = "visible";
+        }else{
+            $scope.myvalidname = "hidden";
+        }
+        if(!$scope.myForm.email.$touched || $scope.myForm.email.$invalid){
+            $scope.myvalidemail = "visible";
+        }else{
+            $scope.myvalidemail = "hidden";
+        }
+        if(!$scope.myForm.address1.$touched || $scope.myForm.address1.$invalid){
+            $scope.myvalidaddress1 = "visible";
+        }else{
+            $scope.myvalidaddress1 = "hidden";
+        }
+        if(!$scope.myForm.zip.$touched || $scope.myForm.zip.$invalid){
+            $scope.myvalidzip = "visible";
+        }else{
+            $scope.myvalidzip = "hidden";
+        }
+        if(!$scope.myForm.city.$touched || $scope.myForm.city.$invalid){
+            $scope.myvalidcity = "visible";
+        }else{
+            $scope.myvalidcity = "hidden";
+        }
+        if(!$scope.myForm.state.$touched || $scope.myForm.state.$invalid){
+            $scope.myvalidstate = "visible";
+        }else{
+            $scope.myvalidstate = "hidden";
+        }
+        if(!$scope.myForm.country.$touched || $scope.myForm.country.$invalid){
+            $scope.myvalidcountry = "visible";
+        }else{
+            $scope.myvalidcountry = "hidden";
+        }
+
+
+
+
         if($scope.myForm.$valid) {
             console.log("form is valid");
           
@@ -16,6 +63,9 @@ app.controller('myc', function ($scope) {
         div.innerHTML = "Hello:  "+newname+"<br>"+"Your email: "+newemail+"<br>";
         document.getElementById("mybox").appendChild(div);
         } 
+
+        $scope.myvalid = "hidden";
+      
     }
 });
 /*
