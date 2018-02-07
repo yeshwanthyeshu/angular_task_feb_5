937db8079fc33e704621e1445d6403b77561b526
c_ymelpati <c_ymelpati@IM-PC-242.innomindshyd.com>
2018-02-07 12:48:45 +0530

    Converted to pure angular
    Converted to pure angular.

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 var app = angular.module("myApp",[]);
-function thopfun(value){
-    console.log(value);
-    console.log(myboxelements[0].name);
-    document.getElementById("name").value = myboxelements[value-2].name;
-    document.getElementById("email").value = myboxelements[value-2].email;
-}
-var myboxelements=[];
-app.controller('myc', function ($scope,$window) {
+app.controller('myc', function ($scope,$window,$compile) {
     $scope.myvalidname = "hidden";
     $scope.myvalidemail = "hidden";
     $scope.myvalidaddress1 = "hidden";
@@ -16,8 +9,11 @@ app.controller('myc', function ($scope,$window) {
     $scope.myvalidcountry = "hidden";
    
     $scope.generateid = 1;
-   
-   
+    $scope.myfun = function(){
+        console.log("button clicked");
+    };
+    $scope.anmyboxelements=[];    
+    $scope.count = 1;
     $scope.submit = function(){
         
         if(!$scope.myForm.name.$touched || $scope.myForm.name.$invalid){
@@ -58,76 +54,45 @@ app.controller('myc', function ($scope,$window) {
        
 
         if($scope.myForm.$valid) {
-        console.log("form is valid");
-        console.log("form is left")
-        var div = document.createElement("div");
-        div.className = "jumbotron jumbotron-fluid";
-        div.id = $scope.generateid;
-        $scope.generateid = $scope.generateid +1;
-        div.style.width = "100%";
-        div.style.height = "100%";
-        div.style.background =  "#e9b6bb";
-        div.style.color = "white";
-        var newname = document.getElementById("name").value;
-        var newemail = document.getElementById("email").value;
-        var newaddress1 = document.getElementById("address1").value;
-        var newaddress2 = document.getElementById("address2").value;
-        var newzip = document.getElementById("zip").value;
-        var newcity = document.getElementById("city").value;
-        var newstate = document.getElementById("state").value;
-        var newcountry = document.getElementById("country").value;
-        console.log($window.myboxelements);
-        $scope.mynewangulabox = {
-            "name": newname,
-            "email": newemail
-        };
-        $window.myboxelements.push($scope.mynewangulabox);
-        console.log($scope.mynewangulabox);
-        console.log($window.myboxelements);
-        div.innerHTML = "Hello:  "+newname+"<button id="+$scope.generateid+" "+"class='editdetails' onclick='thopfun(this.id)'>edit</button><br>"+"Your email: "+newemail+"<br>"
-        +"address:"+newaddress1+" "+newaddress2+" "+newzip+" "+newcity+" "+newstate+" "+newcountry;
-        document.getElementById("mybox").appendChild(div);
+            console.log("form is valid");
+            var div = document.createElement("div");
+            div.className = "jumbotron jumbotron-fluid";
+            div.id = $scope.generateid;
+            $scope.generateid = $scope.generateid +1;
+            div.style.width = "100%";
+            div.style.height = "100%";
+            div.style.background =  "#e9b6bb";
+            div.style.color = "white";
+            var newname = document.getElementById("name").value;
+            var newemail = document.getElementById("email").value;
+            var newaddress1 = document.getElementById("address1").value;
+            var newaddress2 = document.getElementById("address2").value;
+            var newzip = document.getElementById("zip").value;
+            var newcity = document.getElementById("city").value;
+            var newstate = document.getElementById("state").value;
+            var newcountry = document.getElementById("country").value;
+            console.log($window.myboxelements);
+            $scope.mynewangulabox = {
+                "name": newname,
+                "email": newemail
+            };
+            $scope.anmyboxelements.push($scope.mynewangulabox);
+            console.log($scope.mynewangulabox);
+            console.log($window.myboxelements);
+            $scope.angthopfun = function(event){
+                $scope.value = event.target.id;
+                console.log("button id "+event.target.id);
+                console.log("the id is: "+$scope.value);
+                console.log($scope.anmyboxelements[0].name);
+                document.getElementById("name").value = $scope.anmyboxelements[$scope.value-2].name;
+                document.getElementById("email").value;
+            };
+            div.innerHTML = "Hello:  "+newname+"<button id="+$scope.generateid+" "+"class='editdetails' data='{{button.id}}' data-ng-click='angthopfun($event)'>edit</button><br>"+"Your email: "+newemail+"<br>"
+            +"address:"+newaddress1+" "+newaddress2+" "+newzip+" "+newcity+" "+newstate+" "+newcountry;
+            var cdiv =$compile(div)($scope);
+            angular.element(document.getElementById("mybox")).append(cdiv);
         } 
-
-        $scope.thopfun = function(value){
-            console.log("the id is: "+value);
-        };
       
     }
+   
 });
-/*
-function MyCtrl($scope){
-    $scope.submit = function(){
-        console.log("form is left")
-        var div = document.createElement("div");
-        div.className = "jumbotron jumbotron-fluid";
-        div.style.width = "100%";
-        div.style.height = "100px";
-        div.style.background =  "#e9b6bb";
-        div.style.color = "white";
-        var newname = document.getElementById("name").value;
-        var newemail = document.getElementById("email").value;
-        div.innerHTML = "Hello:  "+newname+"<br>"+"Your email: "+newemail+"<br>";
-        
-        document.getElementById("mybox").appendChild(div);
-     
-    } 
-  };
-
-
-
-/**
-function myFunction() {
-    var div = document.createElement("div");
-    div.className = "jumbotron jumbotron-fluid";
-    div.style.width = "100%";
-    div.style.height = "100px";
-    div.style.background =  "#e9b6bb";
-    div.style.color = "white";
-    var newname = document.getElementById("name").value;
-    var newemail = document.getElementById("email").value;
-    div.innerHTML = "Hello:  "+newname+"<br>"+"Your email: "+newemail+"<br>";
-    
-    document.getElementById("mybox").appendChild(div);
-}
-*/
